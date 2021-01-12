303f9814d35258f5e9ab468794c67b23f2f5267e
JantoK <JantoK@foxmail.com>
2021-01-12 19:50:48 +0800

    feat(vite.config.ts):init config, add antd and babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,50 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+const path = require('path');
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  root: path.resolve(__dirname),
+  alias: [
+    {
+      find: "/@",
+      replacement: path.resolve(__dirname, './src')
+    }
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      }
+    }
+  },
+  server: {
+    open: false,
+    cors: true,
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        keep_infinity: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+        strict: true,
+        manualChunks(id) {
+          if (id.includes('/node_modules/')) {
+            const expansions: any[] = [];
+            if (expansions.some(exp => id.includes(`/node_modules/${exp}`))) {
+              return 'expansion'
+            } else {
+              return 'vendor'
+            }
+          }
+        }
+      }
+    }
+  },
 })
