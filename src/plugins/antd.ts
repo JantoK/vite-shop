526a8984515cfb1079ddc3680ea0b21b3c31b4b2
JantoK <JantoK@foxmail.com>
2021-01-14 17:34:27 +0800

    feat(assets):add welcome assets

--- src/plugins/antd.ts
+++ src/plugins/antd.ts
@@ -1,7 +1,10 @@
 import { App } from 'vue';
-import { Button } from 'ant-design-vue';
+import { Button, Form, Input, Card } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
 export function setupAntd(app: App<Element>) {
-    app.use(Button);
+    app.use(Button)
+       .use(Input)
+       .use(Card)
+       .use(Form);
 }
